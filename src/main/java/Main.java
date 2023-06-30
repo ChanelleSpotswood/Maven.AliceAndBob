6fce0081f3f23c9695dcf5d8b4e5960e891b5adf
Chanelle <chanelle@Zipcoders-MacBook-Pro-103.local>
2023-06-30 15:10:51 -0400

    Submitting progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Chanelle's Name Game!");
+        System.out.println("what is your name");
+        Scanner in = new Scanner(System.in);
+        String username = in.nextLine();
+        String alice = "Alice";
+        String bob = "Bob";
 
+        if (alice.equals(username)) {
+            System.out.println("correct");
+        }
     }
 }
